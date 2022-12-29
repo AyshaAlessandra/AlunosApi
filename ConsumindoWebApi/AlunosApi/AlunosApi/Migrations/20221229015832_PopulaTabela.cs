79f3cc90c9d90e61e85cb7194274d9d0893fbe35
Aysha Alessandra <aysha.alessandra.oliver@gmail.com>
2022-12-28 23:41:32 -0300

    Criando models, context e migrations

---
+++ ConsumindoWebApi/AlunosApi/AlunosApi/Migrations/20221229015832_PopulaTabela.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AlunosApi.Migrations
+{
+    public partial class PopulaTabela : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Alunos",
+                columns: new[] { "Id", "Email", "Idade", "Nome" },
+                values: new object[] { 1, "mariaEduarda@gmail.com", 23, "Maria Eduarda" });
+
+            migrationBuilder.InsertData(
+                table: "Alunos",
+                columns: new[] { "Id", "Email", "Idade", "Nome" },
+                values: new object[] { 2, "caioRodrigues@gmail.com", 23, "Caio Rodrigues" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Alunos",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Alunos",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}
---
+++ ConsumindoWebApi/AlunosApi/AlunosApi/Models/Aluno.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AlunosApi.Models
+{
+    public class Aluno
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MinLength(5, ErrorMessage = "O nome deve ser maior!")]
+        [StringLength(80, ErrorMessage = "O nome deve ser menor!")]
+        public string Nome { get; set; }
+
+        [Required]
+        [EmailAddress(ErrorMessage = "Formato de E-mail errado.")]
+        [StringLength(100, ErrorMessage = "O E-mail deve ser menor")]
+        public string Email { get; set;}
+
+        [Required]
+        public int Idade { get; set;}
+    }
+}
