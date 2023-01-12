3b4866eea2476ebdf3a617a1b0e2a542faba6463
Aysha Alessandra <aysha.alessandra.oliver@gmail.com>
2023-01-12 12:24:15 -0300

    Estruturando página novo Aluno

--- React_Cliente/clientreact/src/routes.js
+++ React_Cliente/clientreact/src/routes.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Route, Routes} from 'react-router-dom';
 import Login from "./pages/Login";
 import Alunos from "./pages/Alunos";
+import NovoAluno from "./pages/NovoAluno";
 
 export default function Rout() {
     return (
@@ -9,6 +10,7 @@ export default function Rout() {
             <Routes>
                 <Route exact path="/" element={< Login/>} />
                 <Route path="/alunos" element={< Alunos/>} />
+                <Route path="/aluno/novo/:alunoId" element={< NovoAluno/>} />
             </Routes>
         </BrowserRouter>
     );
