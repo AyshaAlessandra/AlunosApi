87bac610523a773d30ad9d3de921ae18197cabba
Aysha Alessandra <aysha.alessandra.oliver@gmail.com>
2023-01-12 22:15:14 -0300

    Integração do login com a api.

--- React_Cliente/clientreact/src/pages/Login/index.js
+++ React_Cliente/clientreact/src/pages/Login/index.js
@@ -1,21 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './styles.css';
+import api from '../../services/api';
+import { useNavigate } from 'react-router-dom';
+
 import logoImage from '../../assets/login.png'
 
 export default function Login() {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const history = useNavigate();
+
+    async function login(event) {
+        event.preventDefault();
+
+        const data = {
+            email, password
+        };
+
+        try {
+
+            const response = await api.post('api/account/loginuser', data);
+
+            localStorage.setItem('email', email);
+            localStorage.setItem('token', response.data.token);
+            localStorage.setItem('expiration', response.data.expiration);
+
+            history('/alunos');
+        } catch (error) {
+            alert('O login falhou ' + error)
+        }
+    }
+
     return (
-        <div className='login-body'>
-            <div className='login-container'>
-                <section className='form'>
-                    <img src={logoImage} alt='login' id='img1' />
-                    <form>
-                        <h1>Cadastro de Alunos</h1>
-                        <input type='email' placeholder='Email'></input>
-                        <input type='password' placeholder='Password'></input>
-                        <button className='button' type='submit'>Login</button>
-                    </form>
-                </section>
-            </div>
+        <div className="login-container">
+            <section className="form">
+                <img src={logoImage} alt="Login" id="img1" />
+                <form onSubmit={login}>
+                    <h1>Cadastro de Alunos</h1>
+
+                    <input placeholder="Email"
+                        value={email}
+                        onChange={e => setEmail(e.target.value)}
+                    />
+                    <input type="password" placeholder="Password"
+                        value={password}
+                        onChange={e => setPassword(e.target.value)}
+                    />
+                    <button class="button" type="submit">Login</button>
+                </form>
+            </section>
         </div>
     )
 }
