48788b0cfb19985ff5f2232e113dc16efeb50cfa
Aysha Alessandra <aysha.alessandra.oliver@gmail.com>
2023-01-16 11:20:18 -0300

    Implementação da inclusão e edição de alunos.

--- React_Cliente/clientreact/src/pages/Alunos/index.js
+++ React_Cliente/clientreact/src/pages/Alunos/index.js
@@ -42,6 +42,14 @@ export default function Alunos() {
         }
     }
 
+    async function editAluno(id) {
+    try {
+        history(`/aluno/novo/${id}`);
+    } catch (error) {
+        alert('Não foi possível editar o aluno.')
+    }
+    }
+
     return (
         <div className="aluno-container">
             <header>
@@ -66,7 +74,7 @@ export default function Alunos() {
                         <b>Email: </b>{aluno.email}<br /><br />
                         <b>Idade: </b>{aluno.idade}<br /><br />
 
-                        <button title="button">
+                        <button onClick={()=> editAluno(aluno.id)} title="button">
                             <FiEdit size='25' color="#17202a" />
                         </button>
 
